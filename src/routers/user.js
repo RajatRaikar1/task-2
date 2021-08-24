97ebe005f7df1cf0e9359493b7f8d92e24fb5446
RajatRaikar1 <88779767+RajatRaikar1@users.noreply.github.com>
2021-08-24 18:23:17 +0530

    Create user.js

---
+++ src/routers/user.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const User = require('../models/usermodel')
+const router = new express.Router()
+
+router.post('/signup', async (req, res) => {
+    const user = new User(req.body)
+    try{
+        await user.save()
+        res.status(201).send(user)
+    }catch(e){
+        res.status(400).send(e)
+    }
+
+})
+
+
+
+module.exports = router
